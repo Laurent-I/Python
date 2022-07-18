556a9cad448b8ebc0c9626a5b533892805eabcd7
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-18 17:31:42 +0200

    OOP with class attributes in Python

---
+++ oop.py
@@ -0,0 +1,25 @@
+#OOP
+
+class PlayerCharacter:
+    #Class Attributes
+    membership = True
+    def __init__(self, name='Anonymous', age=0):
+        if(age>18):
+            self.name = name
+            self.age = age
+
+    def shout(self):
+        print(f'My name is {self.name}')
+
+    def run(self):
+        print("run")
+        return 'done'
+    # @classmethod
+    # def adding_thing(num1, num2):
+    #     return num1 + num2
+
+player1 = PlayerCharacter('Laurent', 19)
+player1.attack=50
+print(player1.shout())
+
+
