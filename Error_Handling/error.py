321b0f55ca614b2d6c36a4f07dbb1a257400c0f8
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-22 17:57:32 +0200

    Handling mulitple errors

--- Error_Handling/error.py
+++ Error_Handling/error.py
@@ -1,12 +1,21 @@
 # Error Handling
-while True:
+# while True:
+#     try:
+#         age = int(input('What is your age \n'))
+#         print(age)
+#     except ValueError:
+#         print('Please enter a valid age Number')
+#     except ZeroDivisionError:
+#         print('Please enter age higher than zero.')
+#     else:
+#         print('Thank you mate!')
+#         break
+
+def sum_two(num1, num2):
     try:
-        age = int(input('What is your age \n'))
-        print(age)
-    except ValueError:
-        print('Please enter a valid age Number')
-    except ZeroDivisionError:
-        print('Please enter age higher than zero.')
-    else:
-        print('Thank you mate!')
-        break
+        return num1 + num2
+    except (TypeError, ZeroDivisionError) as err:
+        print(f'{err}')
+
+
+print(sum_two('1', 2))
