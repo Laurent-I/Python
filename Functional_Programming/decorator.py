df2f7b33a4c2dbca25ddd18fc004fef2684f5775
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-22 17:11:56 +0200

    Decorators in python

--- Functional_Programming/decorator.py
+++ Functional_Programming/decorator.py
@@ -1,21 +1,21 @@
-# # Decorator: supercharge functions: A Function that wraps another Function and enhances it
-# def my_decorator(func):
-#     def wrap_func():
-#         print('******')
-#         func()
-#         print('******')
-#
-#     return wrap_func
-#
-#
-# @my_decorator
-# def hello():
-#     print('Hello!!!')
-#
-#
-# @my_decorator
-# def bye():
-#     print('See ya Later!!!')
-#
-#
-# hello()
+# Decorator: supercharge functions: A Function that wraps another Function and enhances it
+def my_decorator(func):
+    def wrap_func():
+        print('******')
+        func()
+        print('******')
+
+    return wrap_func
+
+
+@my_decorator
+def hello():
+    print('Hello!!!')
+
+
+@my_decorator
+def bye():
+    print('See ya Later!!!')
+
+
+hello()
