733c86e2e5776a131545f21969ba0057953d091b
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-22 17:27:32 +0200

    Decorators usage to to calculate performance run-time

---
+++ Functional_Programming/performance_decorator.py
@@ -0,0 +1,22 @@
+# Decorator
+from time import time
+
+
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args, **kwargs)
+        t2 = time()
+        print(f'It took {t2 - t1} seconds to run')
+        return result
+
+    return wrapper
+
+
+@performance
+def long_time():
+    for i in range(100):
+        print(i*5)
+
+
+long_time()
