add60f75c7a2dd9d56df2d2feef11de0435dfcb7
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-22 17:16:15 +0200

    Decorators pattern with *args and **kwargs in python

--- Functional_Programming/decorator.py
+++ Functional_Programming/decorator.py
@@ -1,21 +1,36 @@
 # Decorator: supercharge functions: A Function that wraps another Function and enhances it
+# def my_decorator(func):
+#     def wrap_func():
+#         print('******')
+#         func()
+#         print('******')
+#
+#     return wrap_func
+#
+#
+# @my_decorator
+# def hello():
+#     print('Hello!!!')
+#
+#
+# @my_decorator
+# def bye():
+#     print('See ya Later!!!')
+#
+#
+# hello()
+
+# Decorator Pattern
 def my_decorator(func):
-    def wrap_func():
-        print('******')
-        func()
-        print('******')
+    def wrap_func(*args, **kwargs):
+        func(*args, **kwargs)
 
     return wrap_func
 
 
 @my_decorator
-def hello():
-    print('Hello!!!')
+def hello(greeting, emoji=';)'):
+    print(greeting, emoji)
 
 
-@my_decorator
-def bye():
-    print('See ya Later!!!')
-
-
-hello()
+hello('Hiiiiii!!!!!')
