630b6f119eecfe7c464d420e3ad973cb7edc8040
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-25 00:42:01 +0200

    Building the fibonacci sequence generato using generators

--- Generators/fibonacci_numbers.py
+++ Generators/fibonacci_numbers.py
@@ -1,4 +1,4 @@
-# Using Generator
+# Using Generators to create the fibonacci numbers sequence generator
 def fib(number):
     a = 0
     b = 1
@@ -13,18 +13,18 @@ for x in fib(100):
     print(x)
 
 
-# Using List
-def fib2(number):
-    a = 0
-    b = 1
-    result = []
-    for i in range(number):
-        result.append(a)
-        temp = a
-        a = b
-        b = temp + b
-    return result
-
-
-print(fib2(20))
-print(__name__)
+# Using List to create the fibonacci numbers sequence generator
+# def fib2(number):
+#     a = 0
+#     b = 1
+#     result = []
+#     for i in range(number):
+#         result.append(a)
+#         temp = a
+#         a = b
+#         b = temp + b
+#     return result
+#
+#
+# print(fib2(20))
+# print(__name__)
