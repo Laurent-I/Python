6e69943b498df477554107315bebfc5424bde2fd
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-25 00:39:23 +0200

    Building our own for loop using iter and next functions in python

--- Generators/For.py
+++ Generators/For.py
@@ -1,4 +1,4 @@
-# Implementation of our for loop
+# Implementation of our own for loop using the iter and next functions.
 def special_for(iterable):
     iterator = iter(iterable)
     while True:
@@ -7,6 +7,8 @@ def special_for(iterable):
             print(next(iterator))
         except StopIteration:
             break
+
+# Use the StopIteration to handle exceptions
 
 
 special_for([1, 2, 3, 4, 6, 7, 8, 9, 0])
