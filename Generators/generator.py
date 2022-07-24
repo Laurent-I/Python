faccb7c521a1abef40ad97ee990c41f59fbcad78
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-25 00:36:29 +0200

    Learning all about generators

--- Generators/generator.py
+++ Generators/generator.py
@@ -1,8 +1,11 @@
-# Generator is an iterable
+# Generator is an iterable used to save more memory
+# Generators are more performance than lists and save memory so they are faster
 def generator_function(num):
     for i in range(num):
         yield i * 2
 
+
+# Yield method used to move to the next iteration in a generator
 
 g = generator_function(100)
 next(g)
