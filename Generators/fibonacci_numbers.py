78891827621629262fd5cbfa56a04144fb18710e
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-25 00:42:51 +0200

    Building the fibonacci sequence generato using generators and lists

--- Generators/fibonacci_numbers.py
+++ Generators/fibonacci_numbers.py
@@ -14,17 +14,17 @@ for x in fib(100):
 
 
 # Using List to create the fibonacci numbers sequence generator
-# def fib2(number):
-#     a = 0
-#     b = 1
-#     result = []
-#     for i in range(number):
-#         result.append(a)
-#         temp = a
-#         a = b
-#         b = temp + b
-#     return result
-#
-#
-# print(fib2(20))
-# print(__name__)
+def fib2(number):
+    a = 0
+    b = 1
+    result = []
+    for i in range(number):
+        result.append(a)
+        temp = a
+        a = b
+        b = temp + b
+    return result
+
+
+print(fib2(20))
+print(__name__)
