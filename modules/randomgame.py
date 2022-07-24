f963338eefa99cf724b64b4a0864e005100d995c
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-25 00:29:52 +0200

    A Guessing game using built in modules and error handling

--- modules/randomgame.py
+++ modules/randomgame.py
@@ -1,21 +1,21 @@
-# from random import randint
-# import sys
-# # generate a number 1 - 10
-# answer = randint(int(sys.argv[1]), int(sys.argv[2]))
-# # input from user?
-# # check that input is a number 1 to 10
-# while True:
-#     try:
-#         guess = int(input('Guess a number 1 to 10:  '))
-#         if 1 < guess < 11:
-#             if guess == answer:
-#                 print('You guessed the number')
-#                 break
-#         else:
-#             print('Input a number 1 to 10')
-#     except ValueError:
-#         print('Please enter a number')
-#         break
-#
-# # check if the number is the right guess.
-# # ask again
+from random import randint
+import sys
+# generate a number 1 - 10
+answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+# input from user?
+# check that input is a number 1 to 10
+while True:
+    try:
+        guess = int(input('Guess a number 1 to 10:  '))
+        if 1 < guess < 11:
+            if guess == answer:
+                print('You guessed the number')
+                break
+        else:
+            print('Input a number 1 to 10')
+    except ValueError:
+        print('Please enter a number')
+        break
+
+# check if the number is the right guess.
+# ask again
