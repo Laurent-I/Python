0d020df1ee74c86fea4669cd2117453ab05166bf
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-27 00:35:16 +0200

    Unit testing our random game we had built before using the unittest

--- Testing/exercise.py
+++ Testing/exercise.py
@@ -1,23 +1,23 @@
-# from random import randint
-#
-#
-# def run_guess(guess, answer):
-#     if 1 < guess < 11:
-#         if guess == answer:
-#             print('You guessed the number!!!')
-#             return True
-#     else:
-#         print('Input a number 1 to 10')
-#         return False
-#
-#
-# if __name__ == '__main__':
-#     answer = randint(1, 10)
-#     while True:
-#         try:
-#             guess = int(input('Guess a number 1 to 10:  '))
-#             if run_guess(guess, answer):
-#                 break
-#         except ValueError:
-#             print('Please enter a number')
-#             continue
+from random import randint
+
+
+def run_guess(guess, answer):
+    if 1 < guess < 11:
+        if guess == answer:
+            print('You guessed the number!!!')
+            return True
+    else:
+        print('Input a number 1 to 10')
+        return False
+
+
+if __name__ == '__main__':
+    answer = randint(1, 10)
+    while True:
+        try:
+            guess = int(input('Guess a number 1 to 10:  '))
+            if run_guess(guess, answer):
+                break
+        except ValueError:
+            print('Please enter a number')
+            continue
--- Testing/exercise_test.py
+++ Testing/exercise_test.py
@@ -1,32 +1,32 @@
-# import unittest
-# import exercise
-#
-#
-# class TestGame(unittest.TestCase):
-#     def test_input(self):
-#         answer = 5
-#         guess = 5
-#         result = exercise.run_guess(answer, guess)
-#         self.assertTrue(result)
-#
-#     def test_input_wrong_guess(self):
-#         answer = 5
-#         guess = 0
-#         result = exercise.run_guess(answer, guess)
-#         self.assertFalse(result)
-#
-#     def test_input_wrong_number(self):
-#         answer = 5
-#         guess = 11
-#         result = exercise.run_guess(answer, guess)
-#         self.assertFalse(result)
-#
-#     def test_input_wrong_type(self):
-#         answer = 5
-#         guess = '11'
-#         result = exercise.run_guess(answer, guess)
-#         self.assertFalse(result)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import unittest
+import exercise
+
+
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        answer = 5
+        guess = 5
+        result = exercise.run_guess(answer, guess)
+        self.assertTrue(result)
+
+    def test_input_wrong_guess(self):
+        answer = 5
+        guess = 0
+        result = exercise.run_guess(answer, guess)
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        answer = 5
+        guess = 11
+        result = exercise.run_guess(answer, guess)
+        self.assertFalse(result)
+
+    def test_input_wrong_type(self):
+        answer = 5
+        guess = '11'
+        result = exercise.run_guess(answer, guess)
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
