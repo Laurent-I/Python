4360fdfd68f5e4a72f0654bac85ca7dd2b1a05fe
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-27 00:20:34 +0200

    Handling file IO errors

--- File_Handling/file_errors.py
+++ File_Handling/file_errors.py
@@ -1,9 +1,9 @@
-# try:
-#     with open('app/sad.txt', mode='r') as my_file:
-#         print(my_file.read())
-# except FileNotFoundError as err:
-#     print('File does not exist')
-#     raise err
-# except IOError as err:
-#     print('IO Error')
-#     raise err
+try:
+    with open('app/sad.txt', mode='r') as my_file:
+        print(my_file.read())
+except FileNotFoundError as err:
+    print('File does not exist')
+    raise err
+except IOError as err:
+    print('IO Error')
+    raise err
