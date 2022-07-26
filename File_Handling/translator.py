fe71246caa0e84697a2f0c2c22412f7e723aa126
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-27 00:25:10 +0200

    A simpler file translator using the translate library

--- File_Handling/translator.py
+++ File_Handling/translator.py
@@ -1,14 +1,14 @@
-# from translate import Translator
-#
-# translator = Translator(to_lang='ja')
-#
-# try:
-#     with open('test2.txt', mode='r') as my_file:
-#         text = my_file.read
-#         translation = translator.translate(text)
-#         print(translation)
-#
-#         with open('test-ja.txt', mode='r') as my_file2:
-#             my_file2.write(translation)
-# except FileNotFoundError as err:
-#     print('Check your file path')
+from translate import Translator
+
+translator = Translator(to_lang='ja')
+
+try:
+    with open('test2.txt', mode='r') as my_file:
+        text = my_file.read
+        translation = translator.translate(text)
+        print(translation)
+
+        with open('test-ja.txt', mode='r') as my_file2:
+            my_file2.write(translation)
+except FileNotFoundError as err:
+    print('Check your file path')
