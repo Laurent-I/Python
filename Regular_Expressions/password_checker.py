6d0c8aeb07cba302081240fb95db0f4ed09a1ea8
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-27 00:30:21 +0200

    A password checker using regular expressions in python

--- Regular_Expressions/password_checker.py
+++ Regular_Expressions/password_checker.py
@@ -1,10 +1,10 @@
-# import re
-#
-# # At least 8 Characters long
-# # Contains any sort of letters, numbers $%#@
-#
-# pattern = re.compile(r'[a-zA-Z\d$%#@]{8,}\d')
-# password = 'hfbhfbd223fc$$%6@78'
-#
-# check = pattern.fullmatch(password)
-# print(check)
+import re
+
+# At least 8 Characters long
+# Contains any sort of letters, numbers $%#@
+
+pattern = re.compile(r'[a-zA-Z\d$%#@]{8,}\d')
+password = 'hfbhfbd223fc$$%6@78'
+
+check = pattern.fullmatch(password)
+print(check)
