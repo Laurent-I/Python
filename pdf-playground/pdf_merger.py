c419f23c4941a96a293ce8f803ddb095423c63d1
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 05:41:05 +0200

    A bug Fix

--- pdf-playground/pdf_merger.py
+++ pdf-playground/pdf_merger.py
@@ -8,7 +8,7 @@ def pdf_combiner(pdf_list):
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         merger.append(pdf)
-        merger.write('super.pdf')
+    merger.write('super.pdf')
 
 
 pdf_combiner(inputs)
