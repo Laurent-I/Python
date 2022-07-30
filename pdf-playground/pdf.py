21aa39c5da67d862f5323f963f3a4a8bf32a010a
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 05:25:37 +0200

    Write and saving a pdfÂ

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,6 +1,12 @@
 import PyPDF2
 
-with open('011 twopage.pdf', 'rb') as file:
+with open('011 dummy.pdf', 'rb') as file:
     reader = PyPDF2.PdfFileReader(file)
+    page = reader.getPage(0)
     print(reader.numPages)
+    page.rotateClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open('tilt.pdf', 'wb') as new_file:
+        writer.write(new_file)
 
