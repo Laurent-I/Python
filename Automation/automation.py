a5f121b13426ba5471650cec527c3168f27de323
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-13 09:57:10 +0200

    Selenium basics

--- Automation/automation.py
+++ Automation/automation.py
@@ -3,3 +3,9 @@ from selenium import webdriver
 chrome_browser = webdriver.Chrome('./chromedriver.exe')
 
 chrome_browser.maximize_window()
+chrome_browser.get('https://www.seleniumeasy.com')
+
+assert 'Learn Selenium with Best Practices and Examples | Selenium Easy' in chrome_browser.title
+content = chrome_browser.find_element_by_class_name('content')
+print(content)
+
