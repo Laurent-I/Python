19f9ce5221536469541488effd41286ada45754e
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-13 10:29:52 +0200

    Manipulating data using selenium

--- Automation/automation.py
+++ Automation/automation.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_browser = webdriver.Chrome('./chromedriver.exe')
 
 chrome_browser.maximize_window()
-chrome_browser.get('https://www.seleniumeasy.com')
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 
-assert 'Learn Selenium with Best Practices and Examples | Selenium Easy' in chrome_browser.title
-content = chrome_browser.find_element_by_class_name('content')
-print(content)
+assert 'Selenium Easy Demo' in chrome_browser.title
+show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
+# print(button.get_attribute('innerHTML'))
 
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element(By.ID, 'user-message')
+user_message.clear()
+user_message.send_keys('I am extra cool')
+
+show_message_button.click()
+output_message = chrome_browser.find_element(By.ID, 'display')
+
+assert 'I am extra cool' in output_message.text
